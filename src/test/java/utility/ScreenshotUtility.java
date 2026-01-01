5901d3e15f1932b05b7606879f3078ad45eded78
sayoojya-sv <adhisayoo24@gmail.com>
2026-01-02 00:46:30 +0530

    first commit

--- src/test/java/utility/ScreenshotUtility.java
+++ src/test/java/utility/ScreenshotUtility.java
@@ -2,6 +2,8 @@ package utility;
 
 import java.io.File;
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
@@ -13,7 +15,8 @@ public class ScreenshotUtility {
 	public static void capture(WebDriver driver, String testName) throws IOException  {
 
         File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-        File dest = new File("C:\\Users\\sayoojya\\OneDrive\\Desktop\\SDETCourse\\ICTAK_TECH_ADMIN_PROJECT\\ICTAK_TECHBLOG_ADMIN\\target\\screenshots" + testName + ".png");
+        String timestamp=LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd_HH-mm-ss"));
+        File dest = new File("C:\\Users\\sayoojya\\OneDrive\\Desktop\\SDETCourse\\ICTAK_TECH_ADMIN_PROJECT\\ICTAK_TECHBLOG_ADMIN\\target\\screenshots" + testName + "_" + timestamp + ".png");
         FileUtils.copyFile(src, dest);
         
     }
